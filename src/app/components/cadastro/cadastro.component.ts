681ea19582e24a9618694b87c5afc26dd8e9909b
SamuelRicardos <samuelric4rdo@gmail.com>
2024-10-06 16:50:48 -0300

    Adicao de services para requisicoes(login e cadastro) e ajustes

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface SignupForm {
+  name: FormControl,
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
-
+  cadastroForm!: FormGroup<SignupForm>;
   constructor(
-    private router: Router
+    private router: Router,
+    private loginService: LoginService
   ) {
+    this.cadastroForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  cadastro() {
+    this.loginService.signup(this.cadastroForm.value.name, this.cadastroForm.value.email, this.cadastroForm.value.password).subscribe({
+      next: () => alert("sucesso"),
+      error: () => alert("erro")
+    })
   }
 
   navigate(){
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,21 +1,45 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    ReactiveFormsModule
+  ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(
-    private router: Router
-  ) {
+  loginForm!: FormGroup<LoginForm>;
 
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
   }
+
+  submit(){
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => alert("sucesso"),
+      error: () => alert("erro")
+    })
+  }
+
 
 navigate(){
   this.router.navigate(["cadastro"])
